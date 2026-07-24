d2fc9b3e631e29e42e6b004078615389c41b45e4
Adaliz Membreño <adalizmembreno@gmail.com>
2026-07-24 15:06:27 -0600

    configuration for prod

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import { AppModule } from "./app.module";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  const port = Number(process.env.PORT) || 3001;
+  const frontendUrl = process.env.FRONTEND_URL || "http://localhost:3000";
 
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: frontendUrl,
     methods: ["GET"],
   });
 
-  await app.listen(3001);
+  await app.listen(port, "0.0.0.0");
 }
 
 void bootstrap();
